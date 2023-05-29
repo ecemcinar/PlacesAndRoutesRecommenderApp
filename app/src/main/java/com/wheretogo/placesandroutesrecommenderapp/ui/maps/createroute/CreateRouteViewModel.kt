0d22891b2990e01a796dd3b8fbc591a51129b6a8
ecemcinar <ecemsevvalcinar@gmail.com>
2023-05-29 23:32:04 +0300

    [unnecessary imports removed]

--- app/src/main/java/com/wheretogo/placesandroutesrecommenderapp/ui/maps/createroute/CreateRouteViewModel.kt
+++ app/src/main/java/com/wheretogo/placesandroutesrecommenderapp/ui/maps/createroute/CreateRouteViewModel.kt
@@ -1,27 +1,12 @@
 package com.wheretogo.placesandroutesrecommenderapp.ui.maps.createroute
 
 import android.app.Application
-import android.content.ContentValues
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
-import com.google.android.gms.common.api.ApiException
 import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.net.FetchPlaceResponse
-import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
-import com.wheretogo.placesandroutesrecommenderapp.model.map.LatLng
-import com.wheretogo.placesandroutesrecommenderapp.model.map.Location
-import com.wheretogo.placesandroutesrecommenderapp.model.map.RouteMatrixDestination
-import com.wheretogo.placesandroutesrecommenderapp.model.map.RouteMatrixOrigin
-import com.wheretogo.placesandroutesrecommenderapp.model.map.RouteRequest
-import com.wheretogo.placesandroutesrecommenderapp.model.map.Waypoint
-import com.wheretogo.placesandroutesrecommenderapp.repository.googlemaps.GoogleMapsRepository
-import com.wheretogo.placesandroutesrecommenderapp.repository.placeservice.PlaceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
 @HiltViewModel
 class CreateRouteViewModel @Inject constructor(
@@ -37,15 +22,6 @@ class CreateRouteViewModel @Inject constructor(
 
     fun clearLocationList() {
         _locationList.value?.clear()
-    }
-
-    fun setLocationListPositions() {
-        val newOrigin = _locationList.value?.get(1)
-        _locationList.value?.clear()
-
-        newOrigin?.let {
-            _locationList.value?.add(it)
-        }
     }
 
 
